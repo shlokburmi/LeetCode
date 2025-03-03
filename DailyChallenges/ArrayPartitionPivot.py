dc201116705847cf85033403d4dd509832998e1d
shlokburmi <135860207+shlokburmi@users.noreply.github.com>
2025-03-03 18:48:52 +0530

    Create ArrayPartitionPivot.py

---
+++ DailyChallenges/ArrayPartitionPivot.py
@@ -0,0 +1,3 @@
+#2161. Partition Array According to Given Pivot
+    def pivotArray(self, nums, pivot):
+        return [num for num in nums if num<pivot]+[num for num in nums if num==pivot]+[num for num in nums if num>pivot]
