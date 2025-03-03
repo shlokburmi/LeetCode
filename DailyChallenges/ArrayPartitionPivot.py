9b22cbdd098f7b6da3ddc1a50e91c71612dc1949
shlokburmi <135860207+shlokburmi@users.noreply.github.com>
2025-03-03 18:50:10 +0530

    Update ArrayPartitionPivot.py

--- DailyChallenges/ArrayPartitionPivot.py
+++ DailyChallenges/ArrayPartitionPivot.py
@@ -1,3 +1,4 @@
 #2161. Partition Array According to Given Pivot
     def pivotArray(self, nums, pivot):
+def pivotArray(self, nums, pivot):
         return [num for num in nums if num<pivot]+[num for num in nums if num==pivot]+[num for num in nums if num>pivot]
