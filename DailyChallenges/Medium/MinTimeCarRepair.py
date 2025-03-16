c5b2e99bc8016aa3ce8424c4fb589798fb9702de
shlokburmi <135860207+shlokburmi@users.noreply.github.com>
2025-03-16 10:31:02 +0530

    Create MinTimeCarRepair.py

---
+++ DailyChallenges/Medium/MinTimeCarRepair.py
@@ -0,0 +1,8 @@
+#2594. Minimum Time to Repair Cars
+def repairCars(self, ranks: List[int], cars: int) -> int:
+        def numCarsFixed(minutes :int) -> int:
+          #    r*n^2=minutes
+          # -> n=sqrt(minutes/r)
+          return sum(int(sqrt(minutes//rank))for rank in ranks)
+        return bisect.bisect_left(range(0, min(ranks) * cars**2), cars,key=lambda m: numCarsFixed(m))
+        
