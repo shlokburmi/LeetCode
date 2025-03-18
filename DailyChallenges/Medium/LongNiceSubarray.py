b4269f1d6154382d085caef503d91aa127ab4e18
shlokburmi <135860207+shlokburmi@users.noreply.github.com>
2025-03-18 14:49:45 +0530

    initial commit

---
+++ DailyChallenges/Medium/LongNiceSubarray.py
@@ -0,0 +1,12 @@
+#2401. Longest Nice Subarray
+def longestNiceSubarray(self,nums: List[int]) -> int:
+        ans = 0
+        usedBits = 0
+        l = 0
+        for r, num in enumerate(nums):
+            while usedBits & num:
+                usedBits ^= nums[l]
+                l += 1      
+            usedBits |= num
+            ans = max(ans, r - l + 1)        
+        return ans
